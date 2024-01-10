7be7b86e75b78370c809249641420c1781b35a7b
Emmanuelito18 <emmanuelito18yt@gmail.com>
2024-01-10 01:31:27 -0600

    comit inicial

---
+++ src/main/java/com/emmanuelito18_code/portafolio/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.emmanuelito18_code.portafolio;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
